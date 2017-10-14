23dcb5b3ae20961b94f0ef5cedfc5fd5431029f1
Diego Najar <dignajar@gmail.com>
2017-10-14 18:24:21 +0200

    Update bitcoin address

--- index.php
+++ index.php
@@ -144,7 +144,7 @@
 			<div class="modal-content">
 			<span class="donationClose">&times;</span>
 			<h1>Bitcoins</h1>
-			<p><?php l('bitcoins-paragraph1') ?><br><b>3Dm3pNssSs2gLT1DvDoWznXJj829CEyfej</b></p>
+			<p><?php l('bitcoins-paragraph1') ?><br><b>1HKYBAZ6sA2BKewAXzFV8NjVSYkq6RmQnw</b></p>
 			</div>
 		</div>
 
